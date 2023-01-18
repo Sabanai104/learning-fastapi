9e10e388c877cead4f1fd6ee2a4f65ed95c42808
Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>
2023-01-18 09:40:12 -0300

    learn path params, query params, request body utilizations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,8 +13,33 @@ def index():
         }}
 
 
-@app.get('/about')
-def about():
-    return {
-        'data': 'about page'
-    }
+@app.get('/blog')
+def getBlog(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from db'}
+    else:
+        return {'data': f'{limit} blogs from db'}
+        
+
+@app.get('/blog/unplublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def about(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data': {'1', '2', id}}
+
+class Blog(BaseModel): 
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f"Blog is created with title as {request.title}"}
